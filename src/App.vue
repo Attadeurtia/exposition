69fee5c42076ace2ad359b2abe48c87b0a1cef73
Attadeurtia <Attadeurtia@hotmail.com>
2024-09-15 00:19:33 +0200

    Fix layout issue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <HeaderWithArrow />
-    <ImageWithText />
   </div>
+  <HeaderWithArrow />
+  <ImageWithText />
 </template>
 
 <script>
@@ -14,6 +14,7 @@ export default {
   components: {
     ImageWithText,
     HeaderWithArrow
+
   }
 }
 </script>
