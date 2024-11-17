e5b1e4e72c523669a713e2b623669119bd61ef3e
popos-Attadeurtia <Attadeurtia@vivaldi.net>
2024-11-17 19:15:22 +0100

    Correction des noms de composants et mise à jour des chemins dans le fichier de routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
-import App from './App.vue'
+import App from './App.vue';
 import HomePage from './components/HomePage.vue';
-import NewPage from './components/NewPage.vue'
+import NewPage from './components/NewPage.vue';
 
 import AnneDeBretagne from './components/Duchesses/AnneDeBretagne.vue';
 import MargueriteDeBretagne from './components/Duchesses/MargueriteDeBretagne.vue';
-import FrançoisedAmboise from './components/Duchesses/FrançoisedAmboise.vue';
+import FrancoiseDAmboise from './components/Duchesses/FrançoisedAmboise.vue';
 import JeanneDeFlandres from './components/Duchesses/JeanneDeFlandres.vue';
-import JeanneDePenthievre from './components/Duchesses/JeanneDePenthievre.vue';
+import JeanneDePenthièvre from './components/Duchesses/JeanneDePenthievre.vue';
 import Consigne from './components/ConsignePage.vue';
-
-
 
 export const routes = [
     {path: '/', component: App},
@@ -17,17 +15,8 @@ export const routes = [
     {path: '/newpage', component: NewPage, name: 'newpage'},
     { path: '/Anne-de-Bretagne', component: AnneDeBretagne, name: 'Anne-de-Bretagne' },
     { path: '/Marguerite-de-Bretagne', component: MargueriteDeBretagne, name: 'Marguerite-de-Bretagne' },
-    { path: '/Françoise-d-Amboise', component: FrançoisedAmboise, name: 'Françoise-d-Amboise' },
+    { path: '/Francoise-d-Amboise', component: FrancoiseDAmboise, name: 'Francoise-d-Amboise' },
     { path: '/Jeanne-de-Flandres', component: JeanneDeFlandres, name: 'Jeanne-de-Flandres' },
-    { path: '/Jeanne-de-Penthièvre', component: JeanneDePenthievre, name: 'Jeanne-de-Penthièvre' },
-
-    {
-        path: '/consigne',
-        name: 'Consigne',
-        component: Consigne
-      }
-
-
-    //{path: "/:pathMatch(.*)*", redirect: '/'},
-    
+    { path: '/Jeanne-de-Penthievre', component: JeanneDePenthièvre, name: 'Jeanne-de-Penthievre' },
+    { path: '/consigne', component: Consigne, name: 'Consigne'}    
 ];
