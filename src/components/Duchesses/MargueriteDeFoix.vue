05cbe6c01ee3b1193582a142f27c70aa17f39821
popos-Attadeurtia <Attadeurtia@vivaldi.net>
2024-11-30 18:24:10 +0100

    Ajout d'une section d'exposition pour Marguerite de Foix et refactorisation du chargement des images et des textes Markdown.

--- src/components/Duchesses/MargueriteDeFoix.vue
+++ src/components/Duchesses/MargueriteDeFoix.vue
@@ -3,7 +3,6 @@
 
   <MapComponent />
 
-
   <div class="container">
     <div class="item" v-for="(image, index) in images" :key="index">
       <img :src="image.src" :alt="image.alt">
@@ -17,9 +16,13 @@
         <div class="texte" v-html="image.text"></div>
       </div>
     </div>
-
   </div>
 
+  <div class="container">
+    <div class="item" v-for="(expo, index) in expos" :key="index">
+      <div class="texte" v-html="expo.text"></div>
+    </div>
+  </div>
 
   <button @click="goBack" class="back-button">←</button>
 </template>
@@ -35,37 +38,41 @@ export default {
   components: {
     MapComponent
   },
-
   data() {
     return {
-      images: []
+      images: [],
+      introHtml: ''
     };
   },
   async created() {
-    const context = require.context('@/assets/Duchesses/Marguerite de Foix', false, /\.(jpeg|jpg|png)$/);
-    this.images = context.keys().map((key, index) => ({
-      id: index + 1,
-      src: context(key),
-      title: `Image ${index + 1}`,
-      text: ''
-    }));
-  },
-  mounted() {
-    this.loadMarkdownTexts();
+    await this.loadImages();
+    await this.loadMarkdownTexts();
     this.loadIntroMarkdown();
   },
   methods: {
+    async loadImages() {
+      const context = require.context('@/assets/Duchesses/Marguerite de Foix', false, /\.(jpeg|jpg|png)$/);
+      this.images = await Promise.all(
+        context.keys().map(async (key, index) => ({
+          id: index + 1,
+          src: context(key),
+          title: `Image ${index + 1}`,
+          text: ''
+        }))
+      );
+    },
     async loadMarkdownTexts() {
       const md = new MarkdownIt();
-      for (let image of this.images) {
-        try {
-          // Charger le fichier Markdown correspondant à l'image
-          const response = await import(`@/assets/Duchesses/Marguerite de Foix/${image.id} cartel.md`);
-          image.text = md.render(response.default);
-        } catch (error) {
-          console.error(`Erreur lors du chargement du fichier Markdown pour l'image ${image.id}:`, error);
-        }
-      }
+      await Promise.all(
+        this.images.map(async (image) => {
+          try {
+            const response = await import(`@/assets/Duchesses/Marguerite de Foix/${image.id} cartel.md`);
+            image.text = md.render(response.default);
+          } catch (error) {
+            console.error(`Erreur lors du chargement du fichier Markdown pour l'image ${image.id}:`, error);
+          }
+        })
+      );
     },
     loadIntroMarkdown() {
       const md = new MarkdownIt();
