d78a8152caa8de3a11a27d04355eacbb7f1a3b2b
popos-Attadeurtia <Attadeurtia@vivaldi.net>
2024-12-07 15:56:35 +0100

    remise en ordre aphabetique

--- src/components/GlossaireExpo.vue
+++ src/components/GlossaireExpo.vue
@@ -1,22 +1,36 @@
 <template>
-<div class="Glossaire">
-    <h1>Glossaire</h1>
-    <p>
-    <b>Enluminure</b> : Technique d'illustration de manuscrits médiévaux
-    représentant Marguerite et François II.<br>
-    <br>
-    <b>Gisant</b> : Statue funéraire représentant le défunt étendu., souvent sur un tombeau.<br>
-    <br>
-    <b>Hennin</b> : Coiffure féminine du Moyen Âge, bonnet conique très haut et rigide.<br>
-    <br>
-    <b>Contrapposto</b> : Pose légèrement déhanchée de Jeanne de Flandre sur son sceau, typique de la représentation des
-    femmes au Moyen Âge.<br>
-    <br>
-    <b>Dais</b> : Ouvrage d'architecture et de sculpture en pierre, en métal, de bois sculpté ou de tissus, qui sert à couvrir un trône, un autel, une chaire, ou une statue dans une niche<br>
-    <br>
-    <b>Livre d'heures</b> : Livre liturgique destiné aux fidèles catholiques laïcs permettant de suivre la liturgie des Heures.
-</p>
-</div>
+    <div class="Glossaire">
+        <h1>Glossaire</h1>
+        <p>
+            <b>Contrapposto</b> : Pose légèrement déhanchée de Jeanne de Flandre sur son sceau, typique de la
+            représentation des femmes au Moyen Âge.<br>
+            <br>
+            <b>Dais</b> : Ouvrage d'architecture et de sculpture en pierre, en métal, de bois sculpté ou de tissus, qui
+            sert à couvrir un trône, un autel, une chaire, ou une statue dans une niche<br>
+            <br>
+            <b>Enluminure</b> : Technique d'illustration de manuscrits médiévaux<br>
+            <br>
+            <b>Folio</b> : Feuillet d'un manuscrit, d'un incunable, d'un registre ou d'un livre.
+            <br>
+            <br>
+            <b>Gisant</b> : Statue funéraire représentant le défunt étendu, souvent sur un tombeau.<br>
+            <br>
+            <b>Hennin</b> : Coiffure féminine du Moyen Âge, bonnet conique très haut et rigide.<br>
+            <br>
+
+
+
+            <b>Livre d'heures</b> : Livre liturgique destiné aux fidèles catholiques laïcs permettant de suivre la
+            liturgie des Heures.
+            <br>
+            <br>
+            <b>Peinture frontispice</b> : Illustration placée avant la page de titre, avec laquelle elle est soit
+            confondue, soit associée. Cette image est le plus souvent en lien avec le contenu du livre et possède une
+            dimension documentaire, programmatique ou symbolique.
+            <br>
+            <br>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -29,7 +43,7 @@ export default {
 </script>
 
 <style scoped>
-.Glossaire{
+.Glossaire {
     padding: 12%;
 }
 </style>
