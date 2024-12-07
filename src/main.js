90b3a3e701983d1a5b000cc57ba089807a269c52
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-07 13:28:47 +0100

    test avec un hash

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+/* import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes.js';
@@ -6,13 +6,28 @@ import { routes } from './routes.js';
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
-});
+}); */
 
 /* const router = new VueRouter({
     mode: 'history',
     routes: routes
   }); */
 
+/* 
+createApp(App)
+    .use(router)
+    .mount('#app');
+ */
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { routes } from './routes.js';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
 
 createApp(App)
     .use(router)
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/exposition/' : '/',
+
   chainWebpack: (config) => {
     config.module
       .rule('markdown')
