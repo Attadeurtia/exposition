20ad3a99b5a37d2ce137cafdeaef34deea54e36e
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-10 13:25:52 +0100

    update biblio cathy

--- src/components/Sources/BibliographieGeneral.vue
+++ src/components/Sources/BibliographieGeneral.vue
@@ -1,10 +1,12 @@
 <template>
-    <h1>Bibliographie</h1>
+        <div class="biblio">
+            <h1>Bibliographie</h1>
     <h2>Marguerite de Bretagne</h2>
     <BibliographieMargueriteDeBretagne />
 
     <h2>Jeanne De Flandre</h2>
     <BibliographieJeanneDeFlandre />
+</div>
 </template>
 
 <script>
@@ -21,8 +23,14 @@ export default {
 </script>
 
 <style scoped>
-.csl-bib-body {
+
+.biblio{
     padding: 10%;
-    text-align: start;
 }
+
+h2{
+    margin-top: 12%;
+    margin-bottom: 5%
+}
+
 </style>
