a069e16c0080c3f866d61e86de4706f5f4351248
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-11 10:01:57 +0100

    Retourne la position de haut

--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-/* import { createApp } from 'vue';
-import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import { routes } from './routes.js';
-
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
-}); */
-
-/* const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-  }); */
-
-/* 
-createApp(App)
-    .use(router)
-    .mount('#app');
- */
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
@@ -26,7 +5,11 @@ import { routes } from './routes.js';
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes
+    routes,
+    scrollBehavior() {
+        // Retourne la position de défilement souhaitée
+        return { top: 0 }
+    }
 });
 
 createApp(App)
