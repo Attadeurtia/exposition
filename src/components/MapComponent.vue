cf19ae6d56700b9d5f1ea82750d4f7968c991176
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-11 09:49:18 +0100

    ajout de texte dans la carte

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -19,18 +19,19 @@
           v-for="(point, index) in points"
           :key="index"
           :lat-lng="[point.lat, point.lng]"
-          @click="onMarkerClick(point)"
         >
           <!-- Pop-up to show when marker is clicked -->
           <l-popup>
-            <div>{{ point.name }}</div>
+            <div>
+              {{ point.name }}<br />
+              <a :href="point.route" class="popup-link">Voir plus</a>
+            </div>
           </l-popup>
         </l-marker>
       </l-map>
     </div>
   </div>
 </template>
-
 <script>
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
@@ -49,34 +50,34 @@ export default {
       zoom: 10,
       // Array of points with coordinates, names, and routes
       points: [
-        { name: "Anne de Bretagne", lat: 47.41322, lng: -1.219482, route: '/anne-de-bretagne' },
+        { name: "Folio 28r tiré du Livre d’heures d’Isabelle Stuart", lat: 47.41322, lng: -1.219482, route: '/anne-de-bretagne' },
         { name: "Marguerite de Foix", lat: 47.4135, lng: -1.2190, route: '/Marguerite-de-Foix' },
         { name: "Marguerite De Bretagne", lat: 47.4140, lng: -1.2185, route: '/Marguerite-de-Bretagne' },
         { name: "Jeanne de Flandre", lat: 47.4200, lng: -1.3000, route: '/Jeanne-de-Flandree' },
         { name: "Jeanne-de-Penthievre", lat: 48.4140, lng: -1.3185, route: '/Jeanne-de-Penthievre' },
-
       ],
     };
   },
-  methods: {
-    onMarkerClick(point) {
-      this.$router.push(point.route); // Redirige vers la route de la duchesse
-    },
-  },
 };
 </script>
-
 <style scoped>
-.map-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-}
-
-.map-wrapper {
-  height: 600px;
-  width: 800px;
-}
-</style>
+  .map-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+  }
+  
+  .map-wrapper {
+    height: 600px;
+    width: 800px;
+  }
+  
+  .popup-link {
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  </style>
+  
