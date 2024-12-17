9177c979b1791af947e6564426a797e0c5a6bfd4
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-17 23:08:38 +0100

    maj du glossaire avec les images de léocadie

--- src/components/GlossaireExpo.vue
+++ src/components/GlossaireExpo.vue
@@ -1,74 +1,87 @@
 <template>
     <div class="Glossaire">
         <h1>Glossaire</h1>
-        <p>
-    <b>Auray</b> : Commune française située dans le département du Morbihan, en région Bretagne.<br>
-    <br>
-    <b>Charles de Blois</b> : Neveu du roi de France Philippe VI, est un noble français, baron de Mayenne et
-    seigneur de Guise, devenu comte de Penthièvre en épousant la petite-fille du duc de Bretagne Arthur II,
-    Jeanne de Penthièvre. Prétendant au duché de Bretagne du 30 avril 1341 au 29 septembre 1364.<br>
-    <br>
-    <b>Contrapposto</b> : Pose légèrement déhanchée de Jeanne de Flandre sur son sceau, typique de la représentation Grec, très populaire comme représentation à la renaissance mais déja commun au Moyen Âge.<br>
-    <br>
-    <b>Dais</b> : Ouvrage d'architecture et de sculpture en pierre, en métal, de bois sculpté ou de tissus, qui
-    sert à couvrir un trône, un autel, une chaire, ou une statue dans une niche.<br>
-    <br>
-    <b>Dextre</b> : Sur un blason (écusson, écu, armoiries), la dextre désigne donc la gauche du blason (selon
-    le point de vue du spectateur qui regarde le blason).<br>
-    <br>
-    <b>Duc</b> : Une fonction de commandement.<br>
-    <br>
-    <b>Duché</b> : Seigneurie, principauté à laquelle le titre de duc est attaché.<br>
-    <br>
-    <b>Édouard III</b> (1312-1377) : Roi d'Angleterre et seigneur d'Irlande du 25 janvier 1327 à sa mort.<br>
-    <br>
-    <b>Enluminure</b> : Technique d'illustration de manuscrits médiévaux.<br>
-    <br>
-    <b>Folio</b> : Feuillet d'un manuscrit, d'un incunable, d'un registre ou d'un livre.<br>
-    <br>
-    <b>Gisant</b> : Statue funéraire représentant le défunt étendu, souvent sur un tombeau.<br>
-    <br>
-    <b>Guernesey</b> : Dans les îles Anglo-Normandes, est la plus grande île du bailliage de Guernesey.<br>
-    <br>
-    <b>Hennebont</b> : Commune française située en Bretagne dans l'ouest du Morbihan, en aval du fleuve côtier
-    le Blavet.<br>
-    <br>
-    <b>Hennin</b> : Coiffure féminine du Moyen Âge, bonnet conique très haut et rigide.<br>
-    <br>
-    <b>Jean III</b> : Fut duc de Bretagne (1312-1341), et comte de Richmond (1334-1341).<br>
-    <br>
-    <b>Le Blavet</b> : Fleuve en Bretagne qui coule dans les départements des Côtes-d'Armor et du Morbihan et
-    se jette dans l'océan Atlantique près de Lorient.<br>
-    <br>
-    <b>Livre d'heures</b> : Livre liturgique destiné aux fidèles catholiques laïcs permettant de suivre la
-    liturgie des Heures.<br>
-    <br>
-    <b>Louis II</b> : Louis II de Saluces naît le 29 mars 1438, fils de Louis Ier de Saluces, Marquis de
-    Saluces, et d'Isabelle Paléologue. Il meurt à Gênes le 27 janvier 1504.<br>
-    <br>
-    <b>Missel des Carmes</b> : Livre regroupant les textes des lectures et prières constituant la liturgie de
-    la messe pour tous les jours de l'année.<br>
-    <br>
-    <b>Nevers</b> : Commune du centre de la France, située dans le département de la Nièvre en région
-    Bourgogne-Franche-Comté.<br>
-    <br>
-    <b>Peinture frontispice</b> : Illustration placée avant la page de titre, avec laquelle elle est soit
-    confondue, soit associée. Cette image est le plus souvent en lien avec le contenu du livre et possède une
-    dimension documentaire, programmatique ou symbolique.<br>
-    <br>
-    <b>Philippe VI</b> de Valois (1294-1350) : Roi de France de 1328 à 1350.<br>
-    <br>
-    <b>Reine consort</b> : Une reine consort est l'épouse d'un roi régnant, occupant un rôle symbolique et
-    représentatif sans exercer de pouvoir politique.<br>
-    <br>
-    <b>Rethel</b> : Commune française du département des Ardennes, en région Grand Est.<br>
-    <br>
-    <b>Richemont</b> : Commune française située dans le département de la Moselle, en région Grand Est.<br>
-    <br>
-    <b>Senestre</b> : Sur un blason (écusson, écu, armoiries), la senestre désigne donc la droite du blason
-    (selon le point de vue du spectateur qui regarde le blason).<br>
-</p>
 
+
+        <p>
+            <b>Auray</b> : Commune française située dans le département du Morbihan, en région Bretagne.<br>
+            <br>
+            <b>Charles de Blois</b> : Neveu du roi de France Philippe VI, est un noble français, baron de Mayenne et
+            seigneur de Guise, devenu comte de Penthièvre en épousant la petite-fille du duc de Bretagne Arthur II,
+            Jeanne de Penthièvre. Prétendant au duché de Bretagne du 30 avril 1341 au 29 septembre 1364.<br>
+            <br>
+            <b>Contrapposto</b> : Pose légèrement déhanchée de Jeanne de Flandre sur son sceau, typique de la
+            représentation Grec, très populaire comme représentation à la renaissance mais déja commun au Moyen Âge.<br>
+            <br>
+            <b>Dais</b> : Ouvrage d'architecture et de sculpture en pierre, en métal, de bois sculpté ou de tissus, qui
+            sert à couvrir un trône, un autel, une chaire, ou une statue dans une niche.<br>
+            <br>
+            <b>Dextre</b> : Sur un blason (écusson, écu, armoiries), la dextre désigne donc la gauche du blason (selon
+            le point de vue du spectateur qui regarde le blason).<br>
+            <br>
+            <b>Duc</b> : Une fonction de commandement.<br>
+            <br>
+            <b>Duché</b> : Seigneurie, principauté à laquelle le titre de duc est attaché.<br>
+            <br>
+            <b>Édouard III</b> (1312-1377) : Roi d'Angleterre et seigneur d'Irlande du 25 janvier 1327 à sa mort.<br>
+            <br>
+            <b>Enluminure</b> : Technique d'illustration de manuscrits médiévaux.<br>
+            <br>
+            <b>Folio</b> : Feuillet d'un manuscrit, d'un incunable, d'un registre ou d'un livre.<br>
+            <br>
+            <b>Gisant</b> : Statue funéraire représentant le défunt étendu, souvent sur un tombeau.<br>
+            <br>
+            <b>Guernesey</b> : Dans les îles Anglo-Normandes, est la plus grande île du bailliage de Guernesey.<br>
+            <br>
+            <b>Hennebont</b> : Commune française située en Bretagne dans l'ouest du Morbihan, en aval du fleuve côtier
+            le Blavet.<br>
+            <br>
+            <b>Hennin</b> : Coiffure féminine du Moyen Âge, bonnet conique très haut et rigide.<br>
+            <br>
+            <b>Jean III</b> : Fut duc de Bretagne (1312-1341), et comte de Richmond (1334-1341).<br>
+            <br>
+            <b>Le Blavet</b> : Fleuve en Bretagne qui coule dans les départements des Côtes-d'Armor et du Morbihan et
+            se jette dans l'océan Atlantique près de Lorient.<br>
+            <br>
+            <b>Livre d'heures</b> : Livre liturgique destiné aux fidèles catholiques laïcs permettant de suivre la
+            liturgie des Heures.<br>
+            <br>
+            <b>Louis II</b> : Louis II de Saluces naît le 29 mars 1438, fils de Louis Ier de Saluces, Marquis de
+            Saluces, et d'Isabelle Paléologue. Il meurt à Gênes le 27 janvier 1504.<br>
+            <br>
+            <b>Missel des Carmes</b> : Livre regroupant les textes des lectures et prières constituant la liturgie de
+            la messe pour tous les jours de l'année.<br>
+            <br>
+            <b>Nevers</b> : Commune du centre de la France, située dans le département de la Nièvre en région
+            Bourgogne-Franche-Comté.<br>
+            <br>
+            <b>Peinture frontispice</b> : Illustration placée avant la page de titre, avec laquelle elle est soit
+            confondue, soit associée. Cette image est le plus souvent en lien avec le contenu du livre et possède une
+            dimension documentaire, programmatique ou symbolique.<br>
+            <br>
+            <b>Philippe VI</b> de Valois (1294-1350) : Roi de France de 1328 à 1350.<br>
+            <br>
+            <b>Reine consort</b> : Une reine consort est l'épouse d'un roi régnant, occupant un rôle symbolique et
+            représentatif sans exercer de pouvoir politique.<br>
+            <br>
+            <b>Rethel</b> : Commune française du département des Ardennes, en région Grand Est.<br>
+            <br>
+            <b>Richemont</b> : Commune française située dans le département de la Moselle, en région Grand Est.<br>
+            <br>
+            <b>Senestre</b> : Sur un blason (écusson, écu, armoiries), la senestre désigne donc la droite du blason
+            (selon le point de vue du spectateur qui regarde le blason).<br>
+        </p>
+        <div class="glossaire-image">
+            <img src="@/assets/Duchesses/Jeanne de Penthièvre/Schéma des armes de Jeanne de Penthièvre sur le sceau.jpeg"
+                alt="Schéma des armes de Jeanne de Penthièvre sur le sceau">
+            <i>Schéma des armes de Jeanne de Penthièvre sur le sceau</i>
+        </div>
+
+        <div class="glossaire-image">
+            <img src="@/assets/Duchesses/Jeanne de Penthièvre/Schéma des armoiries.jpeg" alt="Schéma des armoiries">
+
+            <i>Schéma des armes de Jeanne de Penthièvre sur le sceau</i>
+        </div>
     </div>
     <FlecheRetour />
 </template>
@@ -90,5 +103,28 @@ export default {
 <style scoped>
 .Glossaire {
     padding: 10%;
+}
+
+img {
+    width: 100%;
+    /* Ajustez la largeur selon vos besoins */
+    max-width: 80%;
+    /* Limite la largeur maximale de l'image */
+    height: auto;
+    /* Maintient le ratio d'aspect de l'image */
+    display: block;
+    /* Assure que l'image est un bloc pour un meilleur alignement */
+    margin: 0 auto;
+    /* Centre l'image horizontalement */
+
+    margin-right: auto;
+    /* Centre l'image horizontalement */
+    margin-left: 0;
+    /* Aligne l'image à droite */
+}
+
+.glossaire-image {
+
+    margin-top: 50px;
 }
 </style>
