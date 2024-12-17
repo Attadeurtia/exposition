403756b317f56b2cfc92eeca2551af47c1311450
Attadeurtia <Attadeurtia@hotmail.com>
2024-12-17 23:15:56 +0100

    ajout de Penthievre dans la biblio

--- src/components/Sources/BibliographieGeneral.vue
+++ src/components/Sources/BibliographieGeneral.vue
@@ -1,37 +1,46 @@
 <template>
-        <div class="biblio">
-            <h1>Bibliographie</h1>
+    <div class="biblio">
+        <h1>Bibliographie</h1>
 
-            <h2>Ressource principale de l'exposition</h2>
+        <h2>Ressource principale de l'exposition</h2>
 
-            <div class="csl-bib-body" style="line-height: 1.35; ">
-  <div class="csl-entry" style="clear: left; ">
-    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;"></div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">L. Moal, <i>Duchesses: histoire d’un pouvoir au féminin en Bretagne les duchesses de Bretagne de Roscille d’Anjou à Anne de Bretagne, Xe-XVIe siècle</i>. Rennes: Presses universitaires de Rennes, 2021.</div>
-  </div>
-  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_id=urn%3Aisbn%3A978-2-7535-8077-0&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rft.genre=book&amp;rft.btitle=Duchesses%3A%20histoire%20d'un%20pouvoir%20au%20f%C3%A9minin%20en%20Bretagne%20les%20duchesses%20de%20Bretagne%20de%20Roscille%20d'Anjou%20%C3%A0%20Anne%20de%20Bretagne%2C%20Xe-XVIe%20si%C3%A8cle&amp;rft.place=Rennes&amp;rft.publisher=Presses%20universitaires%20de%20Rennes&amp;rft.aufirst=Laurence&amp;rft.aulast=Moal&amp;rft.au=Laurence%20Moal&amp;rft.date=2021&amp;rft.isbn=978-2-7535-8077-0&amp;rft.language=fre"></span>
-</div>
-    
+        <div class="csl-bib-body" style="line-height: 1.35; ">
+            <div class="csl-entry" style="clear: left; ">
+                <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">
+                </div>
+                <div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">L. Moal, <i>Duchesses: histoire d’un
+                        pouvoir au féminin en Bretagne les duchesses de Bretagne de Roscille d’Anjou à Anne de Bretagne,
+                        Xe-XVIe siècle</i>. Rennes: Presses universitaires de Rennes, 2021.</div>
+            </div>
+            <span class="Z3988"
+                title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_id=urn%3Aisbn%3A978-2-7535-8077-0&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rft.genre=book&amp;rft.btitle=Duchesses%3A%20histoire%20d'un%20pouvoir%20au%20f%C3%A9minin%20en%20Bretagne%20les%20duchesses%20de%20Bretagne%20de%20Roscille%20d'Anjou%20%C3%A0%20Anne%20de%20Bretagne%2C%20Xe-XVIe%20si%C3%A8cle&amp;rft.place=Rennes&amp;rft.publisher=Presses%20universitaires%20de%20Rennes&amp;rft.aufirst=Laurence&amp;rft.aulast=Moal&amp;rft.au=Laurence%20Moal&amp;rft.date=2021&amp;rft.isbn=978-2-7535-8077-0&amp;rft.language=fre"></span>
+        </div>
 
-    <h2>Jeanne de Flandre</h2>
-    <BibliographieJeanneDeFlandre />
 
-    <h2>Marguerite de Bretagne</h2>
-    <BibliographieMargueriteDeBretagne />
+        <h2>Jeanne de Penthièvre</h2>
+        <BibliographieJeanneDePenthievre />
 
-    <h2>Marguerite de Foix</h2>
-    <BibliographieMargueriteDeFoix/>
+        <h2>Jeanne de Flandre</h2>
+        <BibliographieJeanneDeFlandre />
 
-    <h2>Anne de Bretagne</h2>
-    <BibliographieAnneDeBretagne/>
-</div>
+        <h2>Marguerite de Bretagne</h2>
+        <BibliographieMargueriteDeBretagne />
 
-<FlecheRetour/>
+        <h2>Marguerite de Foix</h2>
+        <BibliographieMargueriteDeFoix />
+
+        <h2>Anne de Bretagne</h2>
+        <BibliographieAnneDeBretagne />
+    </div>
+
+    <FlecheRetour />
 </template>
 
 <script>
 import FlecheRetour from '../FlecheRetour.vue';
 import BibliographieAnneDeBretagne from './BibliographieAnneDeBretagne.vue';
 import BibliographieJeanneDeFlandre from './BibliographieJeanneDeFlandre.vue';
+import BibliographieJeanneDePenthievre from './BibliographieJeanneDePenthievre.vue';
 import BibliographieMargueriteDeBretagne from './BibliographieMargueriteDeBretagne.vue';
 import BibliographieMargueriteDeFoix from './BibliographieMargueriteDeFoix.vue';
 
@@ -39,6 +48,7 @@ export default {
     name: 'BibliographieGeneral',
     components: {
         BibliographieMargueriteDeBretagne,
+        BibliographieJeanneDePenthievre,
         BibliographieJeanneDeFlandre,
         BibliographieAnneDeBretagne,
         BibliographieMargueriteDeFoix,
@@ -48,14 +58,12 @@ export default {
 </script>
 
 <style scoped>
-
-.biblio{
+.biblio {
     padding: 10%;
 }
 
-h2{
+h2 {
     margin-top: 12%;
     margin-bottom: 5%
 }
-
 </style>
---
+++ src/components/Sources/BibliographieJeanneDePenthievre.vue
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Bibliographie</title>
+</head>
+<body>
+<div class="csl-bib-body" style="line-height: 1.35; ">
+  <div class="csl-entry" style="clear: left; ">
+    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">[1]</div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">G. de&nbsp;Longuemar, «&nbsp;ALP&nbsp;». Consulté le: 17 décembre 2024. [En ligne]. Disponible sur: <a href="http://alp22400.net/">http://alp22400.net/</a></div>
+  </div>
+  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Adc&amp;rft.type=webpage&amp;rft.title=ALP&amp;rft.identifier=http%3A%2F%2Falp22400.net%2F&amp;rft.aufirst=Geoffroy&amp;rft.aulast=de%20Longuemar&amp;rft.au=Geoffroy%20de%20Longuemar"></span>
+  <div class="csl-entry" style="clear: left; ">
+    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">[2]</div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">L. Hablot, «&nbsp;Merléac, chapelle Saint-Jacques (peintures héraldiques) – ArmmA&nbsp;». Consulté le: 17 décembre 2024. [En ligne]. Disponible sur: <a href="https://armma.saprat.fr/monument/merleac-chapelle-saint-jacques-nef-voutes-darcades/">https://armma.saprat.fr/monument/merleac-chapelle-saint-jacques-nef-voutes-darcades/</a></div>
+  </div>
+  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Adc&amp;rft.type=webpage&amp;rft.title=Merl%C3%A9ac%2C%20chapelle%20Saint-Jacques%20(peintures%20h%C3%A9raldiques)%20%E2%80%93%20ArmmA&amp;rft.identifier=https%3A%2F%2Farmma.saprat.fr%2Fmonument%2Fmerleac-chapelle-saint-jacques-nef-voutes-darcades%2F&amp;rft.aufirst=Laurent&amp;rft.aulast=Hablot&amp;rft.au=Laurent%20Hablot"></span>
+  <div class="csl-entry" style="clear: left; ">
+    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">[3]</div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">P. Jacquet, «&nbsp;sceau_type : Jeanne de Penthièvre - sceau - Duchesse de Bretagne - 1355/1378&nbsp;», Sigilla. Consulté le: 17 décembre 2024. [En ligne]. Disponible sur: <a href="https://sigilla.irht.cnrs.fr/4851">https://sigilla.irht.cnrs.fr/4851</a></div>
+  </div>
+  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Adc&amp;rft.type=webpage&amp;rft.title=sceau_type%20%3A%20Jeanne%20de%20Penthi%C3%A8vre%20-%20sceau%20-%20Duchesse%20de%20Bretagne%20-%201355%2F1378&amp;rft.description=Notice%20de%20Jeanne%20de%20Penthi%C3%A8vre%20-%20sceau%20-%20Duchesse%20de%20Bretagne%20-%201355%2F1378&amp;rft.identifier=https%3A%2F%2Fsigilla.irht.cnrs.fr%2F4851&amp;rft.aufirst=Philippe&amp;rft.aulast=Jacquet&amp;rft.au=Philippe%20Jacquet&amp;rft.language=fr"></span>
+  <div class="csl-entry" style="clear: left; ">
+    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">[4]</div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">J.-P. ROLLAND, «&nbsp;Penthièvre, Goëlo, Avaugour, Trégor, Guingamp – Patrimoine de Guingamp&nbsp;». Consulté le: 17 décembre 2024. [En ligne]. Disponible sur: <a href="https://patrimoine-guingamp.net/penthievre-goelo-avaugour-tregor-guingamp/">https://patrimoine-guingamp.net/penthievre-goelo-avaugour-tregor-guingamp/</a></div>
+  </div>
+  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Adc&amp;rft.type=webpage&amp;rft.title=Penthi%C3%A8vre%2C%20Go%C3%ABlo%2C%20Avaugour%2C%20Tr%C3%A9gor%2C%20Guingamp%20%E2%80%93%20Patrimoine%20de%20Guingamp&amp;rft.identifier=https%3A%2F%2Fpatrimoine-guingamp.net%2Fpenthievre-goelo-avaugour-tregor-guingamp%2F&amp;rft.aufirst=Jean-Paul&amp;rft.aulast=ROLLAND&amp;rft.au=Jean-Paul%20ROLLAND"></span>
+  <div class="csl-entry" style="clear: left; ">
+    <div class="csl-left-margin" style="float: left; padding-right: 0.5em;text-align: right; width: 1em;">[5]</div><div class="csl-right-inline" style="margin: 0 .4em 0 1.5em;">S. Vincent-Langlois, <i>Trésors enluminés de la Bretagne au Moyen Âge</i>. Rennes: Éditions Ouest-France, 2018.</div>
+  </div>
+  <span class="Z3988" title="url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_id=urn%3Aisbn%3A978-2-7373-7758-7&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rft.genre=book&amp;rft.btitle=Tr%C3%A9sors%20enlumin%C3%A9s%20de%20la%20Bretagne%20au%20Moyen%20%C3%82ge&amp;rft.place=Rennes&amp;rft.publisher=%C3%89ditions%20Ouest-France&amp;rft.aufirst=St%C3%A9phanie&amp;rft.aulast=Vincent-Langlois&amp;rft.au=St%C3%A9phanie%20Vincent-Langlois&amp;rft.date=2018&amp;rft.tpages=379&amp;rft.isbn=978-2-7373-7758-7"></span>
+</div></body>
+</html>
